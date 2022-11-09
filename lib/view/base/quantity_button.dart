5d0c856726df2bad490cc76646dc274f752606e3
shahkutub <shah.kutub1@gmail.com>
2022-11-09 15:28:59 +0600

    restaurant detail cart

--- lib/view/base/quantity_button.dart
+++ lib/view/base/quantity_button.dart
@@ -11,18 +11,18 @@ class QuantityButton extends StatelessWidget {
     return InkWell(
       onTap: onTap,
       child: Container(
-        height: 22, width: 22,
+        height: 20, width: 20,
         margin: EdgeInsets.symmetric(horizontal: Dimensions.PADDING_SIZE_SMALL),
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          border: Border.all(width: 1, color: isIncrement ? Theme.of(context).primaryColor : Theme.of(context).disabledColor),
-          color: isIncrement ? Theme.of(context).primaryColor : Theme.of(context).disabledColor.withOpacity(0.2),
-        ),
+        // decoration: BoxDecoration(
+        //   shape: BoxShape.circle,
+        //   border: Border.all(width: 1, color: isIncrement ? Theme.of(context).primaryColor : Theme.of(context).disabledColor),
+        //   color: isIncrement ? Theme.of(context).primaryColor : Theme.of(context).disabledColor.withOpacity(0.2),
+        // ),
         alignment: Alignment.center,
         child: Icon(
           isIncrement ? Icons.add : Icons.remove,
           size: 15,
-          color: isIncrement ? Theme.of(context).cardColor : Theme.of(context).disabledColor,
+          color: isIncrement ? Colors.deepOrangeAccent : Colors.deepOrangeAccent,
         ),
       ),
     );
--- lib/view/screens/cart/widget/cart_product_widget_web.dart
+++ lib/view/screens/cart/widget/cart_product_widget_web.dart
@@ -69,7 +69,7 @@ class CartProductWidgetWeb extends StatelessWidget {
           child: Stack(children: [
             Positioned(
               top: 0, bottom: 0, right: 0, left: 0,
-              child: Icon(Icons.delete, color: Colors.white, size: 50),
+              child: Icon(Icons.delete, color: Colors.transparent, size: 20),
             ),
             Dismissible(
               key: UniqueKey(),
